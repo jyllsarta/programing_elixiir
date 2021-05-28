de15a4cb3edf7424721d95b99472bc0f76ef598c
jyllsarta <illstarred.dive@gmail.com>
2021-05-28 15:09:11 +0900

    テーブル以外の実装

--- work/issues/lib/cli.ex
+++ work/issues/lib/cli.ex
@@ -48,14 +48,29 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_desc()
+    |> last(count)
   end
 
   def decode_response({:ok, body}), do: body
   def decode_response({:error, error}) do
     IO.puts("Error fetching from GitHub: #{error["message"]}")
     System.halt(2)
+  end
+
+  def sort_desc(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn a, b ->
+      a["created_at"] >= b["created_at"]
+    end)
+  end
+
+  def last(list, count) do
+  list
+  |> Enum.take(count)
+  |> Enum.reverse
   end
 end
