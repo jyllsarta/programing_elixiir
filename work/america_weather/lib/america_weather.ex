07a3e9dd6f532899e685d4cb76aac0e80f89e956
jyllsarta <illstarred.dive@gmail.com>
2021-06-01 18:03:00 +0900

    miax new america_weather

---
+++ work/america_weather/lib/america_weather.ex
@@ -0,0 +1,18 @@
+defmodule AmericaWeather do
+  @moduledoc """
+  Documentation for `AmericaWeather`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AmericaWeather.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ work/america_weather/test/america_weather_test.exs
@@ -0,0 +1,8 @@
+defmodule AmericaWeatherTest do
+  use ExUnit.Case
+  doctest AmericaWeather
+
+  test "greets the world" do
+    assert AmericaWeather.hello() == :world
+  end
+end
