06257a7808ab12b6f0512280830827352aca812c
jyllsarta <illstarred.dive@gmail.com>
2021-06-02 22:05:31 +0900

    マジで面倒だったXML解析

---
+++ work/america_weather/lib/cli.ex
@@ -0,0 +1,39 @@
+defmodule Americaweather.CLI do
+  @moduledoc """
+  shows america weather
+  """
+  def main(_argv) do
+    process()
+  end
+
+  def process() do
+    fetch()
+    |> decode_response()
+    |> show()
+  end
+
+  def fetch() do
+    HTTPoison.get("https://w1.weather.gov/xml/current_obs/KDTO.xml")
+  end
+
+  # エラー処理はもう一旦いいや
+  def decode_response({_, %{status_code: _status_code, body: body}}) do
+    body
+    |> :binary.bin_to_list()
+  end
+
+  # 何回もスキャンして何回もxpath検索するの絶対重いけどちょっと面倒すぎてしんどいので許して
+  def parse(body, xpath_string) do
+    char_list = xpath_string |> :binary.bin_to_list()
+    {root, _} = :xmerl_scan.string(body)
+    [{:xmlText, _, _, _, text, :text}] = :xmerl_xpath.string(char_list, root)
+    text
+  end
+
+  def show(body) do
+    station = body |> parse("/current_observation/station_id/text()")
+    temp = body |> parse("/current_observation/dewpoint_c/text()")
+    weather = body |> parse("/current_observation/weather/text()")
+    IO.puts("station: #{station} / temp: #{temp} / weather: #{weather}")
+  end
+end
--- work/america_weather/mix.exs
+++ work/america_weather/mix.exs
@@ -4,6 +4,7 @@ defmodule AmericaWeather.MixProject do
   def project do
     [
       app: :america_weather,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
@@ -14,15 +15,25 @@ defmodule AmericaWeather.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :xmerl]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 1.0.0"},
+      {:poison, "~> 3.1.0"},
+      {:ex_doc, "~> 0.24"},
+      {:earmark, "~> 1.4.15"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: AmericaWeather.CLI
     ]
   end
 end
